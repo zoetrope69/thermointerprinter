7bd919aea3d47bf811f9b9c0f1e36bb3ffc65340
Zac Colley <hi@zaccolley.com>
2015-02-05 00:33:52 +0000

    Added last.fm nowplaying printer stream

--- app.js
+++ app.js
@@ -16,7 +16,16 @@ var config = require(__dirname + '/config.json'),
 	moment = require('moment'),
 
 	Forecast = require('forecast.io'),
-	forecast = new Forecast({ APIKey: config.forecastio.apiKey });
+	forecast = new Forecast({ APIKey: config.forecastio.apiKey }),
+
+	LastFmNode = require('lastfm').LastFmNode,
+	lastfm = new LastFmNode({
+		api_key: config.lastfm.apiKey,    // sign-up for a key at http://www.last.fm/api
+		secret: config.lastfm.secret,
+		useragent: 'thermointerprinter'
+	}),
+
+	request = require('request');
 
 var app = express();
 
@@ -49,7 +58,7 @@ app.get('/instagram', function (req, res){
 	var input = __dirname + '/instagram.jpg';
 
 	gm(input)
-		.resize(384)
+		.resize(384, 384)
 		.monochrome()
 		.orderedDither('All', '1x1')
 		.write(__dirname + '/public/images/dithered.jpg', function (err){
@@ -64,20 +73,31 @@ app.get('/instagram', function (req, res){
 
 serialPort.on('open',function() {
 
-	var printer = new thermalPrinter(serialPort, {
-		maxPrintingDots: 7, // 0-255. Max heat dots, Unit (8dots), Default: 7 (64 dots)
-		heatingTime: 100, // 3-255. Heating time, Unit (10us), Default: 80 (800us)
-		heatingInterval: 15, //0-255. Heating interval, Unit (10µs), Default: 2 (20µs)
-	});
+	var opts = {
+		maxPrintingDots: 5, // 0-255. Max heat dots, Unit (8dots), Default: 7 (64 dots)
+		heatingTime: 150, // 3-255. Heating time, Unit (10us), Default: 80 (800us)
+		heatingInterval: 200, //0-255. Heating interval, Unit (10µs), Default: 2 (20µs)
+	};
+
+	var printer = new thermalPrinter(serialPort, opts);
+
+	/*
+		The more max heating dots, the more peak current will cost when printing,
+		the faster printing speed. The max heating dots is 8*(n+1).
+
+		The more heating time, the more density, but the slower printing speed.
+		If heating time is too short, blank page may occur.
+
+		The more heating interval, the more clear, but the slower printing speed.
+	*/
 
 	printer.on('ready', function() {
 
-		// printInsta();
 
-		printMessage({
-			name: 'Mr. Printer',
-			message: 'Printer online!'
-		});
+		console.log('Printer ready...')
+
+		// printInsta();
+		lastfmNowPlayin();
 
 		app.post('/', function(req, res) {
 			var data = {
@@ -89,6 +109,113 @@ serialPort.on('open',function() {
 
 			res.send('Sent!');
 		});
+
+		function lastfmNowPlayin(){
+			var trackStream = lastfm.stream(config.lastfm.username);
+
+			trackStream.on('nowPlaying', function(track) {
+				console.log(track);
+
+
+				console.log('Paused track stream');
+				trackStream.stop();
+
+				// simplify response
+				track.imageUrl = track.image[3]['#text'];
+				track.artist = track.artist['#text'];
+				track.album = track.album['#text'];
+
+				console.log('Now playing: ' + track.name + ' - ' + track.artist);
+				console.log('Image URL: '+track.imageUrl);
+
+				var imageFormat = track.imageUrl.substr(track.imageUrl.lastIndexOf("."));
+				console.log('Image format: '+imageFormat);
+				var imagePath = __dirname + '/public/albumart'+imageFormat;
+
+				// if album art
+				if(track.imageUrl !== ''){
+
+					console.log('Dithering album art');
+
+					var printWidth = 384,
+						borderSize = 2,
+						imageSize = printWidth - (borderSize * 2);
+
+					gm(request(track.imageUrl), "input"+imageFormat)
+
+						.resize(imageSize, imageSize)
+
+						.sharpen(5)
+						.monochrome()
+
+						.orderedDither('All', '1x1')
+
+						.borderColor('black')
+						.border(borderSize, borderSize)
+
+						.write(imagePath, function(err) {
+							if (err) throw err;
+
+							var info = track.artist + ' - ' +  track.album;
+
+							// truncate with a ... ending
+							if(info.length > 32){
+								var ending = '...';
+								info = info.substr(0, info.length - 1 - ending.length ) + ending;
+							}
+
+							printer
+								.horizontalLine(32)
+								.printLine(info)
+
+								.big(true)
+								.printLine(wordwrap(track.name, 16))
+								.big(false)
+								.lineFeed(1)
+
+								.printImage(imagePath)
+
+								.printLine(moment().format('MMMM Do YYYY, h:mm:ss a'))
+								.horizontalLine(32)
+								.lineFeed(1)
+
+								.print(function(err){
+									if (err) throw err;
+
+									console.log('Image printed');
+
+									console.log('Resumed track stream');
+									trackStream.start();
+								});
+
+								function wordwrap( str, width, brk, cut ) {
+
+									brk = brk || '\n';
+									width = width || 75;
+									cut = cut || false;
+
+									if (!str) { return str; }
+
+									var regex = '.{1,' +width+ '}(\\s|$)' + (cut ? '|.{' +width+ '}|.+$' : '|\\S+?(\\s|$)');
+
+									return str.match( RegExp(regex, 'g') ).join( brk );
+
+								}
+						});
+
+				}else{
+
+					console.log('No album art :¬(');
+
+					console.log('Resumed track stream');
+					trackStream.start();
+
+				}
+
+			});
+
+			trackStream.start();
+		}
 
 		app.get('/weather', function(req, res) {
 
@@ -225,7 +352,7 @@ serialPort.on('open',function() {
 				});
 		}
 
-		function printImage(imagePath){
+		function printImage(imagePath, callback){
 			printer
 				.lineFeed(2)
 				.printImage(imagePath)
@@ -234,6 +361,7 @@ serialPort.on('open',function() {
 					if (err) throw err;
 
 					console.log(imagePath);
+					callback();
 				});
 		}
 
